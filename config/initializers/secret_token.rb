70db9357799564ddef03ecf3efea4267a066f0ff
adeng21 <adeng21@gmail.com>
2014-03-28 15:54:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogPark::Application.config.secret_key_base = '938da7fc94059789748a3589e1324b6c63da03c432739aa72ba2268d9b2099be8ac4008174b43aa97c35c2c89712259e0b0400a8b1ab7ab15617c7cd5be9dc0b'
---
+++ spec/Features/dog_registration_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'dog registration', %Q{
+  As a dog owner
+  I want to register my dog
+  So that I can connect with other dog lovers
+} do
+
+  #Acceptance Criteria
+  #An owner must specify their first name, last name, email address, and dog's name
+  #Upon adding my entry, I can see a list of other registered dog owners.
+  #An owner's full_name can be requested
+
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
